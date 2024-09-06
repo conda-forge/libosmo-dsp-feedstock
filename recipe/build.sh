843a4ce7f55085923a600e495cbd5adc1cd93499
Ryan Volz <ryan.volz@gmail.com>
2024-09-06 18:16:54 -0400

    Back to m2-pkg-config since it has pkg.m4 which we need, fix path

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,14 +14,14 @@ configure_args=(
 
 if [[ "$target_platform" == win-64 ]]; then
   # set default include and library dirs for Windows build
-#  export CPPFLAGS="$CPPFLAGS -isystem $PREFIX/include"
-#  export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+  export CPPFLAGS="$CPPFLAGS -isystem $PREFIX/include"
+  export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
   # to pick up pkg-config macros
-#  autoreconf_args+=(
-#    -I "$BUILD_PREFIX/Library/mingw-w64/share/aclocal"
-#  )
+  autoreconf_args+=(
+    -I "$BUILD_PREFIX/Library/usr/share/aclocal"
+  )
   # so we can find packages with pkg-config (e.g. fftw)
-#  export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+  export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
   # so we can make shared libs without overzealous checking
   export lt_cv_deplibs_check_method=pass_all
 fi
